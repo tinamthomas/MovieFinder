162ce2d716d74c34f4666b5e2900177ac707d38a
Tina Maria Thomas <tinainink@gmail.com>
2020-02-10 17:57:34 -0500

    Tina | DB migrations + Fastify server

---
+++ imbd.data/migrations/20200209190019-actors.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  db.createTable('actors', {
+    id: { type: 'string', primaryKey: true },
+    name: 'string',
+    birthyear: 'string',
+    deathyear: 'string',
+    primaryProfession: {type: 'text[]'},
+    knownForTitles: {type: 'text[]'}
+  });
+  return null;
+};
+
+exports.down = function(db) {
+  db.dropTable('actors');
+};
+
+exports._meta = {
+  "version": 1
+};
