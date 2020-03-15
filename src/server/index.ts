0f873793ddd66c773bd8a3b97be7fa73f8d4a9b4
Tina Maria Thomas <tinainink@gmail.com>
2020-03-14 21:51:17 -0400

    Tina | Client code webpack config and build

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,19 @@
 import movieListHandler from "./handlers/movies";
+const path = require('path')
 
 const fastify = require('fastify')({ logger: true })
 
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, '../../dist'),
+  prefix: '/public/',
+});
+
+fastify.get('/', function (req, reply) {
+  reply.sendFile('index.html') // serving path.join(__dirname, 'public', 'myHtml.html') directly
+})
 
 // Declare a route
-fastify.get('/', movieListHandler)
+fastify.get('/movies', movieListHandler)
 
 // Run the server!
 const start = async () => {
